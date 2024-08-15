4e6b11b593ad7ac16d0cc32df2537a29c5288274
Changxiang Gao <gao654@purdue.edu>
2024-08-15 09:24:28 -0400

    fix bugs to run retriever

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ with open("README.md") as f:
 setup(
     name="dpr",
     version="1.0.0",
+    packages=["dpr"],
     description="Facebook AI Research Open Domain Q&A Toolkit",
     url="https://github.com/facebookresearch/DPR/",
     classifiers=[
